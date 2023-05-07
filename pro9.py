76c98111f8066abc65c91a66be2c3190240a9198
An Nguyen <ankimnguyen92@gmail.com>
2023-05-07 13:07:50 -0500

    Starting Out with Python

--- pro9.py
+++ pro9.py
@@ -1,9 +1,25 @@
 """Exception handing"""
 
 def main():
-    read_file = open(input('Enter file name: '), 'r')
+    try:
+        read_file = open(input('Enter file name: '), 'r')
+        total = 0
+        count = 0
+        try:
+            for line in read_file:
+                total += int(line.strip('\n'))
+                count +=1
+            average = total / count
+            print(f'The average is {average}')
 
-    read_file.close()
+        except ValueError:
+            print(f'Calculation error')
+
+        read_file.close()
+    except IOError:
+        print("Input/Output Error. Files might not exist in the directory.")
+        print(f'The file is {read_file}')
+
 
 
 if __name__ == "__main__":
